dfc71252484895e0009be08d9606b21a21efd75a
Mizarc <contact@mizarc.dev>
2025-02-03 00:08:13 +1000

    Add Bukkit implementation for messaging service

--- src/main/kotlin/dev/mizarc/waystonewarps/application/services/MessagingService.kt
+++ src/main/kotlin/dev/mizarc/waystonewarps/application/services/MessagingService.kt
@@ -3,7 +3,7 @@ package dev.mizarc.waystonewarps.application.services
 import java.util.*
 
 interface MessagingService {
-    fun sendChatMessage(playerId: UUID, message: String)
-    fun sendActionMessage(playerId: UUID, message: String)
-    fun sendAlertMessage(playerId: UUID, message: String)
+    fun sendChatMessage(playerId: UUID, message: String): Result<Unit>
+    fun sendActionMessage(playerId: UUID, message: String): Result<Unit>
+    fun sendTitleMessage(playerId: UUID, titleText: String, subtitleText: String): Result<Unit>
 }
