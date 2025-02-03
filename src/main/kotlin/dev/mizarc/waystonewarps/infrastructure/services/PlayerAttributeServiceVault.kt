7cc9f4b009b3f387700bd52c1cffb608b0634113
Mizarc <contact@mizarc.dev>
2025-02-03 11:58:40 +1000

    Rework PlayerLimitRepository to PlayerAttributeService
    Since this technically represents a service more than a repository, it
    has been moved to a service in the application layer. The name has also
    been changed from Limit to Attribute since it encompasses more than just
    limits.

--- src/main/kotlin/dev/mizarc/waystonewarps/infrastructure/services/PlayerAttributeServiceVault.kt
+++ src/main/kotlin/dev/mizarc/waystonewarps/infrastructure/services/PlayerAttributeServiceVault.kt
@@ -1,18 +1,18 @@
-package dev.mizarc.waystonewarps.infrastructure.persistence.players
+package dev.mizarc.waystonewarps.infrastructure.services
 
-import dev.mizarc.waystonewarps.domain.players.PlayerLimitRepository
+import dev.mizarc.waystonewarps.application.services.PlayerAttributeService
 import dev.mizarc.waystonewarps.infrastructure.persistence.Config
 import net.milkbowl.vault.chat.Chat
 import org.bukkit.Bukkit
 import java.util.*
 
 /**
- * A Vault API repository implementation that handles read only player limits.
+ * A Vault API player attribute implementation that handles read only player limits.
  *
  * This implementation uses the Vault API to store limits via a third party provider (e.g. LuckPerms)
  */
-class PlayerLimitRepositoryVault(private val config: Config,
-                                 private val metadata: Chat): PlayerLimitRepository {
+class PlayerAttributeServiceVault(private val config: Config,
+                                  private val metadata: Chat): PlayerAttributeService {
     override fun getWaystoneLimit(playerId: UUID): Int =
         metadata.getPlayerInfoInteger(
             Bukkit.getServer().worlds[0].name, Bukkit.getPlayer(playerId),
