fa6dfba86c9c8abdbdc0fd72dc7010b2725d721c
Mizarc <contact@mizarc.dev>
2025-02-08 14:19:49 +1000

    Update dependencies to work with 1.21.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,14 +29,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     shadow("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.insert-koin:koin-core:4.0.2")
     implementation("co.aikar:idb-core:1.0.0-SNAPSHOT")
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("com.github.stefvanschie.inventoryframework:IF:0.10.11")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    implementation("com.github.stefvanschie.inventoryframework:IF:0.10.19")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
 }
 
 java {
