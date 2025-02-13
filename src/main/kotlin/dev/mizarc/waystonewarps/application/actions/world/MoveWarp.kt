7f19c94bac5f45c68e1e488de6f632fe8b9f0711
Mizarc <contact@mizarc.dev>
2025-02-13 21:45:26 +1000

    Add MoveWarp use case
    This will be used by the listener to move warps to a different location.

---
+++ src/main/kotlin/dev/mizarc/waystonewarps/application/actions/world/MoveWarp.kt
@@ -0,0 +1,24 @@
+package dev.mizarc.waystonewarps.application.actions.world
+
+import dev.mizarc.waystonewarps.application.results.MoveWarpResult
+import dev.mizarc.waystonewarps.application.services.StructureBuilderService
+import dev.mizarc.waystonewarps.domain.positioning.Position3D
+import dev.mizarc.waystonewarps.domain.warps.WarpRepository
+import java.util.UUID
+
+class MoveWarp(private val warpRepository: WarpRepository,
+               private val structureBuilderService: StructureBuilderService) {
+    fun execute(playerId: UUID, warpId: UUID, position: Position3D): MoveWarpResult {
+        val warp = warpRepository.getById(warpId) ?: return MoveWarpResult.WARP_NOT_FOUND
+
+        if (warp.playerId != playerId) {
+            return MoveWarpResult.NOT_OWNER
+        }
+
+        structureBuilderService.despawnStructure(warp)
+        warp.position = position
+        structureBuilderService.spawnStructure(warp)
+        warpRepository.update(warp)
+        return MoveWarpResult.SUCCESS
+    }
+}
