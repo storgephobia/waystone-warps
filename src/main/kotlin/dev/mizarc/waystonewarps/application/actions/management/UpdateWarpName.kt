ae6e34c2f5db7a2ea6f7ee6d8bcfc74ec8ec8d47
Mizarc <contact@mizarc.dev>
2025-02-26 19:53:41 +1000

    Change UpdateWarpName to take into account more results
    This now allows you to capture if the name is blank, already taken, or
    if the warp is not found.

--- src/main/kotlin/dev/mizarc/waystonewarps/application/actions/management/UpdateWarpName.kt
+++ src/main/kotlin/dev/mizarc/waystonewarps/application/actions/management/UpdateWarpName.kt
@@ -1,13 +1,18 @@
 package dev.mizarc.waystonewarps.application.actions.management
 
+import dev.mizarc.waystonewarps.application.results.UpdateWarpNameResult
 import dev.mizarc.waystonewarps.domain.warps.WarpRepository
 import java.util.*
 
 class UpdateWarpName(private val warpRepository: WarpRepository) {
-    fun execute(warpId: UUID, name: String): Result<Unit> {
-        val warp = warpRepository.getById(warpId) ?: return Result.failure(Exception("Warp not found"))
+    fun execute(warpId: UUID, playerId: UUID, name: String): UpdateWarpNameResult {
+        if (name.isBlank()) return UpdateWarpNameResult.NAME_BLANK
+        if (warpRepository.getByName(playerId, name) != null) {
+             return UpdateWarpNameResult.NAME_ALREADY_TAKEN
+        }
+        val warp = warpRepository.getById(warpId) ?: return UpdateWarpNameResult.WARP_NOT_FOUND
         warp.name = name
         warpRepository.update(warp)
-        return Result.success(Unit)
+        return UpdateWarpNameResult.SUCCESS
     }
 }
