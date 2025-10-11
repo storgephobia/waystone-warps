263e600196cf0147db2f09c093881136204412d1
Mizarc <contact@mizarc.dev>
2025-10-11 14:53:29 +1000

    Add new build deployment to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,29 @@
+import java.util.Properties
+
 plugins {
     kotlin("jvm") version "2.0.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use { inputStream ->
+        localProperties.load(inputStream)
+    }
+}
+
+fun getProperty(name: String): String {
+    val localValue = localProperties.getProperty(name)
+    if (localValue != null) {
+        return localValue
+    }
+    return providers.gradleProperty(name).getOrElse("")
+}
+
 group = "dev.mizarc"
-version = "0.3.4"
+version = "0.3.5"
 
 repositories {
     mavenLocal()
@@ -35,7 +54,7 @@ dependencies {
     implementation("co.aikar:idb-core:1.0.0-SNAPSHOT")
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("com.github.stefvanschie.inventoryframework:IF:0.11.1")
+    implementation("com.github.mizarc:IF:0.11.4-d")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     implementation("me.xdrop:fuzzywuzzy:1.3.1")
 }
@@ -50,4 +69,13 @@ tasks.test {
 
 tasks.shadowJar {
     archiveClassifier = null
+}
+
+tasks.register<Copy>("deploy") {
+    dependsOn(tasks.shadowJar)
+    from(layout.buildDirectory.dir("libs"))
+    println("Target deployment path: ${getProperty("plugin.server.path")}")
+    into(getProperty("plugin.server.path"))
+    rename { fileName -> "${rootProject.name}-${version}.jar" }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
