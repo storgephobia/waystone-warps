faeb965f735bcda7e4cb491595ac75356f6fb750
Johan Marwin Manio <storgephobia@gmail.com>
2026-07-21 22:52:13 +0800

    Update build.gradle.kts
    god knows at this point

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,11 +48,11 @@ repositories {
         url = uri("https://jitpack.io")
     }
     maven {
-        url = uri("https://repo.glaremasters.me/repository/towny/")
-    }
-    maven {
         name = "Multiverse"
         url = uri("https://repo.onarandombox.com/public")
+    }
+    maven {
+        url = uri("https://repo.glaremasters.me/repository/towny/")
     }
 }
 
@@ -73,8 +73,8 @@ dependencies {
     implementation("com.github.stefvanschie.inventoryframework:IF:0.12.0")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("me.xdrop:fuzzywuzzy:1.4.0")
-    compileOnly("com.palmergames.bukkit.towny:towny:0.103.1.0")
     compileOnly("org.mvplugins.multiverse.inventories:multiverse-inventories:5.3.5")
+    compileOnly("com.palmergames.bukkit.towny:towny:0.103.1.0")
 }
 
 java {
