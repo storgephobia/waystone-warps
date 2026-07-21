2d97336566b6bdc05c588adcfff16b60080acf6d
Johan Marwin Manio <storgephobia@gmail.com>
2026-07-21 23:28:12 +0800

    Update Towny dependency version in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,7 @@ dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("me.xdrop:fuzzywuzzy:1.4.0")
     compileOnly("org.mvplugins.multiverse.inventories:multiverse-inventories:5.3.5")
-    implementation("com.github.TownyAdvanced:Towny:0.103.1.0")
-    // compileOnly ("com.palmergames.bukkit.towny:towny:0.103.0.7")
+    compileOnly("com.github.TownyAdvanced:Towny:0.102.0.13")
 }
 
 java {
