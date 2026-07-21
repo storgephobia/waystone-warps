6cb33d0f18a6db607360b7f91979eaadf2893ea7
Johan Marwin Manio <storgephobia@gmail.com>
2026-07-21 23:20:18 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("com.github.stefvanschie.inventoryframework:IF:0.12.0")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("me.xdrop:fuzzywuzzy:1.4.0")
-    compileOnly("com.github.TownyAdvanced:Towny:0.103.0.7")
+    implementation("com.github.TownyAdvanced:Towny:0.103.0.7")
     // compileOnly ("com.palmergames.bukkit.towny:towny:0.103.0.7")
     compileOnly("org.mvplugins.multiverse.inventories:multiverse-inventories:5.3.5")
 }
